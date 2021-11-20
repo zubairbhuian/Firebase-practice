bfaff39d52d05ec3ee51888769dd394209f92850
[zubair bhuian] <[zubairbhuian@gmail.com]>
2021-11-20 15:38:42 +0600

    e4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import { AppBar, Avatar, Button, Toolbar, Typography } from '@material-ui/core';
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useState } from 'react';
+import firebaseConfig from './firebase.config';
+
+
+const [user ,setuser] =useState({
+  isSignedIn:false,
+  photo:''
+})
 
 
 
+initializeApp(firebaseConfig);
+// handel
+const handelSignIn = ()=>{
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth();
+signInWithPopup(auth, provider)
+  .then((result) => {
+    const photoURL = result.user;
+    const isSignedIn ={
+      
+      photo:photoURL
+    }
+    useState(isSignedIn)
+    console.log(result.user.photoURL);
+  
+  }).catch((error) => {
+    console.log(error);
+  });
 
-
+}
 export default function App(){
  return (
     <AppBar position="fixed" color="primary">
@@ -12,9 +40,9 @@ export default function App(){
           Xixer
         </Typography>
         <div style={{display:'flex'}}>
-        <Avatar style={{marginRight:'20px'}} alt="Remy Sharp" src=".." />
+        <Avatar style={{marginRight:'20px'}} alt="Remy Sharp" src={user} />
           <Button  variant="contained" color="primary">Sign out</Button>
-          <Button  variant="contained" color="primary">Sign in</Button>
+          <Button onClick={handelSignIn} variant="contained" color="primary">Sign in</Button>
         </div>
       </Toolbar>
     </AppBar>
