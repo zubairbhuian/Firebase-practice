4ef9343205790c0339af516f012317107e184a93
[zubair bhuian] <[zubairbhuian@gmail.com]>
2021-11-21 07:03:08 +0600

    this is demo commit

--- src/App.js
+++ src/App.js
@@ -1,38 +1,35 @@
 import { AppBar, Avatar, Button, Toolbar, Typography } from '@material-ui/core';
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 import firebaseConfig from './firebase.config';
 
 
+// const [user ,setuser] = useState(1)
 
 
 
 initializeApp(firebaseConfig);
+// handel
+const handelSignIn = ()=>{
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth();
+signInWithPopup(auth, provider)
+  .then((result) => {
+    // const photoURL = result.user;
+    // const isSignedIn ={
+      
+    //   photo:photoURL
+    // }
+    // setuser(isSignedIn)
+    console.log(result.user.photoURL);
+  
+  }).catch((error) => {
+    console.log(error);
+  });
+
+}
 export default function App(){
-  const [user ,setuser] = useState({
-    isSignIn:false,
-    photo:''
-  })
-  
-  const handelSignIn = ()=>{
-    const provider = new GoogleAuthProvider();
-    const auth = getAuth();
-  signInWithPopup(auth, provider)
-    .then((result) => {
-      const photoURL = result.user;
-      const isSignedIn ={
-        isSigIn:true,
-        photo:photoURL
-      }
-      setuser(isSignedIn)
-      console.log(result.user.photoURL);
-    
-    }).catch((error) => {
-      console.log(error);
-    });
-  
-  }
  return (
     <AppBar position="fixed" color="primary">
       <Toolbar  style={{display:"flex",justifyContent:"space-between"}}>
@@ -40,7 +37,7 @@ export default function App(){
           Xixer
         </Typography>
         <div style={{display:'flex'}}>
-        <Avatar style={{marginRight:'20px'}} alt="m" src='user.photo'/>
+        <Avatar style={{marginRight:'20px'}} alt="Remy Sharp" src='..' />
           <Button  variant="contained" color="primary">Sign out</Button>
           <Button onClick={handelSignIn} variant="contained" color="primary">Sign in</Button>
         </div>
